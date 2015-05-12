b0ac413829af4f67237f87865b42b9bddda80e78
Alex Lee <aintaer@aintaer.com>
2015-05-12 01:53:58 -0400

    sax: modify from sax-js@1.1.0
    Taking isaacs/sax-js@1.1.0 as the base, modify it to allow for HTML5
    void element parsing (elements that are not self-closed, but must
    exhibit the self closing behavior).

--- Magic.js
+++ Magic.js
@@ -1,110 +1,110 @@
 import View from 'nbd/View';
 import extend from 'nbd/util/extend';
 import async from 'nbd/util/async';
-import sax from 'sax';
+import sax from './sax';
 import vdom from 'virtual-dom';
 
 const _parser = sax.parser();
 const tagstack = [];
 
-const blocking = Symbol('debounced');
+const blocking = '__debounced__'; // Symbol('debounced');
 
 function debounce(fn) {
-  if (fn[blocking]) { return; }
-  const retval = fn.call(this);
-  fn[blocking] = true;
-  async(() => delete fn[blocking]);
-  return retval;
+	if (fn[blocking]) { return; }
+	const retval = fn.call(this);
+	fn[blocking] = true;
+	async(() => delete fn[blocking]);
+	return retval;
 }
 
 let tree;
 
 function isAttribute(name) {
-  return !(/^value$/i.test(name));
+	return !(/^value$/i.test(name));
 }
 
 function transformAttributes(properties) {
-  properties.attributes = {};
-  for (let prop in properties) {
-    if (properties.hasOwnProperty(prop) && prop !== 'attributes') {
-      if (prop.toLowerCase() === 'class') {
-        properties.className = properties[prop];
-        delete properties[prop];
-      }
-      if (isAttribute(prop)) {
-        properties.attributes[prop] = properties[prop];
-        delete properties[prop];
-      }
-    }
-  }
+	properties.attributes = {};
+	for (let prop in properties) {
+		if (properties.hasOwnProperty(prop) && prop !== 'attributes') {
+			if (prop.toLowerCase() === 'class') {
+				properties.className = properties[prop];
+				delete properties[prop];
+			}
+			if (isAttribute(prop)) {
+				properties.attributes[prop] = properties[prop];
+				delete properties[prop];
+			}
+		}
+	}
 }
 
 extend(_parser, {
-  onopentag({ name, attributes }) {
-    transformAttributes(attributes);
-    const tag = { name, attributes, children: [] };
-    if (tagstack[tagstack.length - 1]) {
-      tagstack[tagstack.length - 1].children.push(tag);
-    }
-    tagstack.push(tag);
-  },
-  onclosetag() {
-    const tag = tagstack.pop();
-    let vnode = vdom.h(tag.name, tag.attributes, tag.children);
-    const last = tagstack[tagstack.length - 1];
-    if (last) {
-      last.children.splice(last.children.lastIndexOf(tag), 1, vnode);
-    }
-    else {
-      tree = vnode;
-    }
-  },
-  ontext(text) {
-    if (!text.trim()) {
-      return;
-    }
-    if (tagstack[tagstack.length - 1]) {
-      tagstack[tagstack.length - 1].children.push(text);
-    }
-  },
-  onend() {
-    tagstack.length = 0;
-  }
+	onopentag({ name, attributes }) {
+		transformAttributes(attributes);
+		const tag = { name, attributes, children: [] };
+		if (tagstack[tagstack.length - 1]) {
+			tagstack[tagstack.length - 1].children.push(tag);
+		}
+		tagstack.push(tag);
+	},
+	onclosetag() {
+		const tag = tagstack.pop();
+		let vnode = vdom.h(tag.name, tag.attributes, tag.children);
+		const last = tagstack[tagstack.length - 1];
+		if (last) {
+			last.children.splice(last.children.lastIndexOf(tag), 1, vnode);
+		}
+		else {
+			tree = vnode;
+		}
+	},
+	ontext(text) {
+		if (!text.trim()) {
+			return;
+		}
+		if (tagstack[tagstack.length - 1]) {
+			tagstack[tagstack.length - 1].children.push(text);
+		}
+	},
+	onend() {
+		tagstack.length = 0;
+	}
 });
 
-export default class Magic extends View.extend({
-  init(...args) {
-    this._super(...args);
-    if (this._model.on) {
-      this.listenTo(this._model, 'all', () => debounce.call(this, this.render));
-    }
-  },
+export default View.extend({
+	init(...args) {
+		this._super(...args);
+		if (this._model.on) {
+			this.listenTo(this._model, 'all', () => debounce.call(this, this.render));
+		}
+	},
 
-  template(data) {
-    return this.mustache && this.mustache(data, this.partials);
-  }
-}) {
-  static domify(html) {
-    tree = null;
-    _parser.write(html).close();
-    return tree;
-  }
+	template(data) {
+		return this.mustache && this.mustache(data, this.partials);
+	}
+}, {
+	domify(html) {
+		tree = null;
+		_parser.write(html).close();
+		return tree;
+	},
 
-  static appendTo(tree, $context) {
-    tree._el = vdom.create(tree);
-    return super.appendTo(tree._el, $context);
-  }
+	appendTo(tree, $context) {
+		tree._el = vdom.create(tree);
+		return View.appendTo(tree._el, $context);
+	},
 
-  static replace(oldtree, newtree) {
-    const patch = vdom.diff(oldtree, newtree);
-    vdom.patch(newtree._el = oldtree._el, patch);
-  }
+	replace(oldtree, newtree) {
+		const patch = vdom.diff(oldtree, newtree);
+		vdom.patch(newtree._el = oldtree._el, patch);
+	},
 
-  static find($root, selector) {
-    return super.find($root._el || $root, selector);
-  }
+	find($root, selector) {
+		return View.find($root._el || $root, selector);
+	},
 
-  static remove($el) {
-    return super.remove($el._el || $el);
-  }
-}
+	remove($el) {
+		return View.remove($el._el || $el);
+	}
+});
